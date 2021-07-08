4b7b2607ca15ae6e1cd2ef295030822837ab7f0c
vithorluc <vithorvarela.academico@gmail.com>
2021-07-08 00:40:29 -0300

    heal player

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -40,7 +40,17 @@ const app = Vue.createApp({
             const attackValue = getRandomvalue(10, 25)
             this.monsterHealth -= attackValue
             this.attackPlayer()
-        }  
+        },
+        healPlayer(){
+            this.currentRound++
+            const healValue = getRandomvalue(8, 20)
+            if (this.playerHealth + healValue > 100){
+                this.playerHealth = 100
+            } else {
+                this.playerHealth += healValue
+            }
+            this.attackPlayer()
+        }
     },
     watch: {
 
