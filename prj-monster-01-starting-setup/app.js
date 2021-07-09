b51e4f79ec64b4fc1f32d269f2dedc8c60bf55dd
vithorluc <vithorvarela.academico@gmail.com>
2021-07-08 21:54:16 -0300

    messages logs

--- prj-monster-01-starting-setup/app.js
+++ prj-monster-01-starting-setup/app.js
@@ -9,14 +9,22 @@ const app = Vue.createApp({
             playerHealth: 100,
             monsterHealth: 100,
             currentRound: 0,
-            winner: null
+            winner: null,
+            logMessages: []
         }
     },
     computed: {
+        
         monsterBarStyles(){
+            if(this.monsterHealth < 0) {
+                return {width: '0%'}
+            }
             return {width: this.monsterHealth + '%'}
         },
         playerBarStyles(){
+            if(this.playerHealth < 0) {
+                return {width: '0%'}
+            }
             return {width: this.playerHealth + '%'}
         },
         mayUseSpecialAttack(){
@@ -24,22 +32,35 @@ const app = Vue.createApp({
         }
     },
     methods: {
+        surrender(){
+            this.winner = 'monster';
+        },
+        startGame(){
+            this.playerHealth = 100
+            this.monsterHealth = 100 
+            this.winner = null 
+            this.currentRound = 0
+            this.logMessages = []
+        },
         attackMonster(){
             this.currentRound++
             const attackValue = getRandomvalue(5, 12)
             this.monsterHealth -= attackValue
             // methods are also merged with global variables accessible from everywhere 
             // into the code
+            this.addLogMessage('player', 'attack', attackValue)
             this.attackPlayer()
         },
         attackPlayer(){
             const attackValue = getRandomvalue(8, 15)
             this.playerHealth -= attackValue
+            this.addLogMessage('monster', 'attack', attackValue)
         },
         specialAttackMonster(){
             this.currentRound++
             const attackValue = getRandomvalue(10, 25)
             this.monsterHealth -= attackValue
+            this.addLogMessage('player', 'special-attack', attackValue)
             this.attackPlayer()
         },
         healPlayer(){
@@ -50,7 +71,15 @@ const app = Vue.createApp({
             } else {
                 this.playerHealth += healValue
             }
+            this.addLogMessage('player', 'heal', healValue)
             this.attackPlayer()
+        },
+        addLogMessage(who, what, value){
+            this.logMessages.unshift({
+                actionBy: who,
+                actionType: what,
+                actionValue: value 
+            })
         }
     },
     watch: {
